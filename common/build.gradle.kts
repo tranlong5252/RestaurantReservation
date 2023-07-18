6fb97735db63efed3da6f591ad5fee3f557dd53a
tranlong5252 <longpham5252@gmail.com>
2023-07-18 21:57:01 +0700

    fix gradle

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -12,7 +12,6 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(project(mapOf("path" to ":common")))
 }
 
 tasks.test {
